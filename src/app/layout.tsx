8f59299acd05135310b2d35d64b750b780882134
Usama Jawad <99108743+usama216@users.noreply.github.com>
2025-09-19 17:51:09 +0500

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,11 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'STAYCONNECTED - Security and Network Solutions',
   description: 'Specialists in providing comprehensive security and network solutions for businesses and homes.',
+  icons: {
+    icon: '/log-dark.png',
+    shortcut: '/log-dark.png',
+    apple: '/log-dark.png',
+  },
 }
 
 export default function RootLayout({
